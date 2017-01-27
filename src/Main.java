183e1c0472e4e919ec4174614ec47cc36bdd5c39
DylanAz <Dyl.deleeuw@gmail.com>
2017-01-27 15:41:24 +0100

    Serveur + debut interface + config serveur

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
-
 public class Main {
 
-	public static void main(String[] args) {
-		String token="";
-		Connection C = new Connection(token);
+	   public static void main(String[] args) {
+	    
+	      /*String host = "127.0.0.1";
+	      int port = 2345;
+	      
+	      TimeServer ts = new TimeServer(host, port);
+	      ts.open();
+	      
+	      System.out.println("Serveur initialisé.");
+	      
+	      Thread t = new Thread(new ClientConnexion(host, port));
+	      
+	      t.start();
+	      
+	      
+	      
+	      
+	      
+	      for(int i = 0; i < 5; i++){
+	         Thread t = new Thread(new ClientConnexion(host, port));
+	         t.start();
+	      }*/
+		   
+		   Connection C = new Connection( "" );
+	   }
 	}
-
-}
