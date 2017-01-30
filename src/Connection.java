371c8dd5c1fec89e0a451f5aa615793ce61677f1
DylanAz <Dyl.deleeuw@gmail.com>
2017-01-30 13:43:02 +0100

    connexion facebook par le jeton

--- src/Connection.java
+++ src/Connection.java
@@ -1,9 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,15 +17,20 @@ import javax.swing.JTextField;
 public class Connection extends JFrame {
 	private JPanel container = new JPanel();
 	private JTextField jtf = new JTextField("");
-	private JLabel label = new JLabel("Ajouter votre Token d'accès : ");
+	private JLabel label = new JLabel("Ajouter votre jeton d'accès : ");
+	private JLabel labeltest = new JLabel("");
 	private JButton b = new JButton("OK");
+	private JButton b2 = new JButton("Obtenir un jeton");
+	private JButton b3 = new JButton("Test");
+	private Facebook f;
 
-	public Connection(String token) { 
+	public Connection(String token, Facebook facebook) { 
 		if (token != "") {
 			this.jtf = new JTextField(token);
 		}
-		this.setTitle("Connection");
-		this.setSize(400, 500);
+		f=facebook;
+		this.setTitle("Connection à Facebook");
+		this.setSize(400, 100);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.setAlwaysOnTop(true);
@@ -34,20 +42,58 @@ public class Connection extends JFrame {
 		jtf.setFont(police);
 		jtf.setPreferredSize(new Dimension(150, 30));
 		jtf.setForeground(Color.BLUE);
-		b.addActionListener(new BoutonListener());
+		b.addActionListener(new BoutonListenerConnection());
+		b2.addActionListener(new BoutonListenerJeton());
+		b3.addActionListener(new BoutonListenerTest());
+		labeltest.setFont(police);
+		labeltest.setForeground(Color.BLUE);
+		
 		top.add(label);
 		top.add(jtf);
 		top.add(b);
+		top.add(b2);
+		top.add(b3);
+		top.add(labeltest);
+
 	    container.add(top, BorderLayout.NORTH);
 		this.setContentPane(top);
 		this.setVisible(true);
-
+	}
+	
+	class BoutonListenerTest implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Test : ");
+			f.setToken(jtf.getText());
+			if(f.Test()){
+				labeltest.setForeground(Color.GREEN);
+				labeltest.setText("Ok");
+			}
+			else{
+				labeltest.setForeground(Color.RED);
+				labeltest.setText("Erreur");
+			}
+				
+				
+		}
 	}
 
-	class BoutonListener implements ActionListener {
+	class BoutonListenerConnection implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Token : " + jtf.getText());
-
+			f.setToken(jtf.getText());
+		}
+	}
+	
+	class BoutonListenerJeton implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Utilisateur envoyé vers https://developers.facebook.com/tools/explorer/");
+			URI uri = URI.create("https://developers.facebook.com/tools/explorer/");
+			try {
+				Desktop.getDesktop().browse(uri);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
 	}
 }
