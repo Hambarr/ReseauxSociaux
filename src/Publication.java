08799370fb097e55e5d27c1c38f220ff48f90e2a
DylanAz <Dyl.deleeuw@gmail.com>
2017-01-30 16:19:48 +0100

    Add files via upload

--- src/Publication.java
+++ src/Publication.java
@@ -1,4 +1,15 @@
+import java.util.Date;
 
 public class Publication {
-
+	public Publication(String auteur, Date date, int nb_com, int nb_like) {
+		super();
+		this.auteur = auteur;
+		this.date = date;
+		this.nb_com = nb_com;
+		this.nb_like = nb_like;
+	}
+	String auteur;
+	Date date;
+	int nb_com;
+	int nb_like;
 }
