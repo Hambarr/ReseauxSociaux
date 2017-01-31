53d2d2eec5b6d4ba70f4b3fbcbea7b0822d59d91
DylanAz <Dyl.deleeuw@gmail.com>
2017-01-31 14:04:23 +0100

    Add files via upload

---
+++ Publication.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Publication {
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public int getNb_com() {
+		return nb_com;
+	}
+
+	public void setNb_com(int nb_com) {
+		this.nb_com = nb_com;
+	}
+
+	public int getNb_like() {
+		return nb_like;
+	}
+
+	public void setNb_like(int nb_like) {
+		this.nb_like = nb_like;
+	}
+
+	public Publication(String auteur, int nb_com, int nb_like) {
+		super();
+		this.auteur = auteur;
+		this.nb_com = nb_com;
+		this.nb_like = nb_like;
+	}
+	
+	public Publication() {
+		
+	}
+	
+	String auteur;
+	int nb_com;
+	int nb_like;
+	
+}
+
